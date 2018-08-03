d22a0811e50b71a0cca24f378eb8a92196850639
King Wilder <king@gizmobeach.com>
2018-08-02 23:14:35 -0700

    Removed an unused import reference.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './home/home.component';
 import { MainLayoutComponent } from './shared/layout/app-layouts/main-layout/main-layout.component';
 
 const routes: Routes = [
